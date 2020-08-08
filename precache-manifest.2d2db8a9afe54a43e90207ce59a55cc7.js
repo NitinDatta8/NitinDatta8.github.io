e51152e2de8b5cc078b54dd18efb6753009b3ee2
Nitin Datta <movvand@gmail.com>
2020-08-08 16:15:58 +0530

    Updates

---
+++ precache-manifest.2d2db8a9afe54a43e90207ce59a55cc7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf222253dfde1459ce7edc97e0d7149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce494e161ba09f631303",
+    "url": "/static/css/3.48a93ee6.chunk.css"
+  },
+  {
+    "revision": "085253e7e24f6704b754",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "8772523ca0fc4ec6cea1",
+    "url": "/static/css/main.0b657d5d.chunk.css"
+  },
+  {
+    "revision": "aa99a1493af3365b9126",
+    "url": "/static/js/2.cd116c3b.chunk.js"
+  },
+  {
+    "revision": "ce494e161ba09f631303",
+    "url": "/static/js/3.1eb25922.chunk.js"
+  },
+  {
+    "revision": "085253e7e24f6704b754",
+    "url": "/static/js/4.42102756.chunk.js"
+  },
+  {
+    "revision": "8772523ca0fc4ec6cea1",
+    "url": "/static/js/main.88783b2e.chunk.js"
+  },
+  {
+    "revision": "cd6c05a122c4b0ad1939",
+    "url": "/static/js/runtime-main.1e7fe4ff.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "989b356092b8589b1b19bce8d254427a",
+    "url": "/static/media/CNN.989b3560.png"
+  },
+  {
+    "revision": "42e6e07c4e7c4e952521c0327b4c4a43",
+    "url": "/static/media/DNN.42e6e07c.png"
+  },
+  {
+    "revision": "f6524bb8025b4ea1fc831b52d744609c",
+    "url": "/static/media/GWD.f6524bb8.png"
+  },
+  {
+    "revision": "68ab541e508cf53f21c721878a72335a",
+    "url": "/static/media/ML.68ab541e.png"
+  },
+  {
+    "revision": "07da3c525291136053f2f6650f93c0b4",
+    "url": "/static/media/MLAZ.07da3c52.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "375796f45b27e44b4f30304d31669fb6",
+    "url": "/static/media/NN.375796f4.png"
+  },
+  {
+    "revision": "d329a811d5cfc573b88790bb6e308fb3",
+    "url": "/static/media/SIH.d329a811.jpeg"
+  },
+  {
+    "revision": "cfe78ac6ff005e18f405274a3a9d4407",
+    "url": "/static/media/SML.cfe78ac6.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "b8e6a6f7acf31c9cb6c946448162c2bc",
+    "url": "/static/media/finisar.b8e6a6f7.png"
+  },
+  {
+    "revision": "75c573469ce7b1741939dff0cdebad14",
+    "url": "/static/media/hackathon.75c57346.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "a797039485998beb9fd8946a42f3a1d0",
+    "url": "/static/media/master.a7970394.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
